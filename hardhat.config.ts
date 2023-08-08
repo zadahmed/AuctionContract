8f1e28cb8394d892eaf9cc2bf47095b98a816d17
Zahid Ahmed <zahidzahmed@gmail.com>
2023-08-08 12:19:59 +0100

    feat: install necessary packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-ethers'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.14",
 };
 
 export default config;
